50a6d006cadd3216cb3c4edd4051774304e42e8f
Madlen Minasyan <mminasya@c2r15s5.42yerevan.am>
2025-03-31 22:16:09 +0400

    elo

--- get_next_line.c
+++ get_next_line.c
@@ -1,175 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mminasya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/31 21:48:59 by mminasya          #+#    #+#             */
+/*   Updated: 2025/03/31 22:00:58 by mminasya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
-#include <stdio.h>
 
-void *myfree(char *line)
+void	*myfree(char *line)
 {
-	if(line)
+	if (line)
 		free(line);
-	return(NULL);
+	return (NULL);
 }
-char *fill_stash(int fd, char *stash)
+
+char	*fill_stash(int fd, char *stash)
 {
-	char buffer[BUFFER_SIZE + 1];
-	ssize_t bytes_read;
-	char *tmp;
+	char	buffer[BUFFER_SIZE + 1];
+	ssize_t	bytes_read;
+	char	*tmp;
 
-	if(BUFFER_SIZE <= 0) 
-		return(NULL);
-
-	if(!stash)
+	if (BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!stash)
 		stash = ft_strdup("");
-	while ( !ft_strchr(stash, '\n') && (bytes_read = read(fd , buffer, BUFFER_SIZE))> 0)
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	while (!ft_strchr(stash, '\n') && bytes_read > 0)
 	{
 		buffer[bytes_read] = '\0';
 		tmp = ft_strjoin(stash, buffer);
-		if(!tmp)
-			return(myfree(stash));
+		if (!tmp)
+			return (myfree(stash));
 		free(stash);
 		stash = tmp;
+		bytes_read = read(fd, buffer BUFFER_SIZE);
 	}
-	if(bytes_read < 0)
+	if (bytes_read < 0)
 		return (myfree(stash));
-	return(stash);
+	return (stash);
 }
-char *the_line(char *stash)
-{
-	char *newline_pos;
 
-	if(!stash || !*stash)
-		return(NULL);
-	
-	newline_pos = ft_strchr(stash, '\n');
-	if(newline_pos)
-		return(ft_substr(stash, 0, (newline_pos - stash) + 1 ));
-	return(ft_substr(stash, 0, ft_strlen(stash)));
-}
-char *update_stash(char *stash)
+char	*the_line(char *stash)
 {
-	char *new_stash;
-	char *newline_pos;
-	
+	char	*newline_pos;
+
+	if (!stash || !*stash)
+		return (NULL);
 	newline_pos = ft_strchr(stash, '\n');
-	if(!newline_pos)
+	if (newline_pos)
+		return (ft_substr(stash, 0, (newline_pos - stash) + 1));
+	return (ft_substr(stash, 0, ft_strlen(stash)));
+}
+
+char	*update_stash(char *stash)
+{
+	char	*new_stash;
+	char	*newline_pos;
+
+	newline_pos = ft_strchr(stash, '\n');
+	if (!newline_pos)
 	{
 		free(stash);
-		return(NULL);
+		return (NULL);
 	}
 	new_stash = ft_strdup(newline_pos + 1);
-	if(!new_stash)
+	if (!new_stash)
 		return (NULL);
-
 	free(stash);
-	return(new_stash);
+	return (new_stash);
 }
-char *get_next_line(int fd)
-{
-	static char *stash;
-	char *line;
 
-	if(fd < 0 || BUFFER_SIZE <= 0 )
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	stash = fill_stash(fd, stash);
 	if (!stash)
-		return(NULL);
+		return (NULL);
 	line = the_line(stash);
 	stash = update_stash(stash);
-
-	return(line);
-	
+	return (line);
 }
+/*
 int main()
 {
         int fd;
         fd = open("test.txt", O_RDONLY);
 	printf("%s",get_next_line(fd));
-		printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-		printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-		printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-		printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-		printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-		printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-		printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-		printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-		printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-		printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-		printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
 
         close(fd);
         return 0;
-}
+}*/
--- get_next_line.h
+++ get_next_line.h
@@ -1,25 +1,35 @@
-#ifndef GET_NEXT_LINE_H
-#	define GET_NEXT_LINE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mminasya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/31 21:48:18 by mminasya          #+#    #+#             */
+/*   Updated: 2025/03/31 22:15:47 by mminasya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-# ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+void	*myfree(char *line);
+char	*fill_stash(int fd, char *stash);
+char	*the_line(char *stash);
+char	*get_next_line(int fd);
+char	*update_stash(char *stash);
+size_t	ft_strlen(char *s);
+char	*ft_strdup(char *s1);
+char	*ft_strchr(char *s, int c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
 
-# endif 
-
-# include <fcntl.h>
-
-void *myfree(char *line);
-char *fill_stash(int fd, char *stash);
-char *the_line(char *stash);
-char *get_next_line(int fd);
-char *update_stash(char *stash);
-size_t ft_strlen(char *s);
-char *ft_strdup(char *s1);
-char *ft_strchr(char *s, int c);
-char *ft_substr(char *s, unsigned int start, size_t len);
-char *ft_strjoin(char *s1, char *s2);
-# endif
-
+#endif
